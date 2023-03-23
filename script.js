d94336cb79b0e004255406266fe8c76d1e00fe40
Gunnar-Gray <gunnargray41@gmail.com>
2023-03-23 05:21:52 -0400

    changes to html,css and javascript. New site

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-document.getElementById('darkModeToggle').addEventListener('change', function() {
-    document.body.classList.toggle('dark-mode', this.checked);
+
+document.addEventListener('DOMContentLoaded', () => {
+  const menuBtn = document.querySelector('.menu-btn');
+  const menuLinks = document.querySelector('.menu-links');
+  const darkModeBtn = document.querySelector('.dark-mode-btn');
+  const body = document.querySelector('body');
+
+  // Navbar menu toggle
+  menuBtn.addEventListener('click', () => {
+    menuLinks.classList.toggle('open');
+    menuBtn.classList.toggle('open');
   });
- 
+
+  // Light/dark mode toggle
+  darkModeBtn.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+    darkModeBtn.classList.toggle('dark-mode');
+  });
+});
+
